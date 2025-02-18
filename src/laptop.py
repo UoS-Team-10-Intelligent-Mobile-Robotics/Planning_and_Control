a6e441b5d4d533f5e7a82d3a832d7586a39ab401
Andrei Sontea <sonteaandrei@gmail.com>
2025-02-18 12:02:11 +0000

    Added .gitignore and changed runtime.ini

--- src/laptop.py
+++ src/laptop.py
@@ -5,41 +5,52 @@ All rights reserved.
 Licensed under the BSD 3-Clause License.
 See LICENSE.md file in the project root for full license information.
 """
+
 import numpy as np
 import argparse
 from datetime import datetime
 import time
 from drivers.aruco_udp_driver import ArUcoUDPDriver
 from zeroros import Subscriber, Publisher
-from zeroros.messages import LaserScan, Vector3Stamped, Pose, PoseStamped, Header, Quaternion
+from zeroros.messages import (
+    LaserScan,
+    Vector3Stamped,
+    Pose,
+    PoseStamped,
+    Header,
+    Quaternion,
+)
 from zeroros.datalogger import DataLogger
 from zeroros.rate import Rate
 from model_feeg6043 import TrajectoryGenerate
 from math_feeg6043 import 12m
 # add more libraries here
 
+
 class LaptopPilot:
     def __init__(self, simulation):
         # network for sensed pose
         aruco_params = {
             "port": 50000,  # Port to listen to (DO NOT CHANGE)
-            "marker_id": 25,  # Marker ID to listen to (CHANGE THIS to your marker ID)            
+            "marker_id": 25,  # Marker ID to listen to (CHANGE THIS to your marker ID)
         }
         self.robot_ip = "192.168.90.1"
-        
+
         # handles different time reference, network amd aruco parameters for simulator
-        self.sim_time_offset = 0 #used to deal with webots timestamps
-        self.sim_init = False #used to deal with webots timestamps
+        self.sim_time_offset = 0  # used to deal with webots timestamps
+        self.sim_init = False  # used to deal with webots timestamps
         self.simulation = simulation
         if self.simulation:
-            self.robot_ip = "127.0.0.1"          
-            aruco_params['marker_id'] = 0  #Ovewrites Aruco marker ID to 0 (needed for simulation)
-            self.sim_init = True #used to deal with webots timestamps
+            self.robot_ip = "127.0.0.1"
+            aruco_params["marker_id"] = (
+                0  # Ovewrites Aruco marker ID to 0 (needed for simulation)
+            )
+            self.sim_init = True  # used to deal with webots timestamps
 
         print("Connecting to robot with IP", self.robot_ip)
         self.aruco_driver = ArUcoUDPDriver(aruco_params, parent=self)
 
-        ############# INITIALISE ATTRIBUTES ##########        
+        ############# INITIALISE ATTRIBUTES ##########
         # path
         self.northings_path = []
         self.eastings_path = []  
@@ -58,27 +69,30 @@ class LaptopPilot:
 
         # wheel speed commands
         self.cmd_wheelrate_right = None
-        self.cmd_wheelrate_left = None 
+        self.cmd_wheelrate_left = None
 
         # encoder/actual wheel speeds
         self.measured_wheelrate_right = None
-        self.measured_wheelrate_left = None   
+        self.measured_wheelrate_left = None
 
         # lidar
         self.lidar_timestamp_s = None
         self.lidar_data = None
-        ###############################################################        
+        ###############################################################
 
         self.datalog = DataLogger(log_dir="logs")
 
-        # Wheels speeds in rad/s are encoded as a Vector3 with timestamp, 
-        # with x for the right wheel and y for the left wheel.        
+        # Wheels speeds in rad/s are encoded as a Vector3 with timestamp,
+        # with x for the right wheel and y for the left wheel.
         self.wheel_speed_pub = Publisher(
             "/wheel_speeds_cmd", Vector3Stamped, ip=self.robot_ip
         )
 
         self.true_wheel_speed_sub = Subscriber(
-            "/true_wheel_speeds",Vector3Stamped, self.true_wheel_speeds_callback,ip=self.robot_ip,
+            "/true_wheel_speeds",
+            Vector3Stamped,
+            self.true_wheel_speeds_callback,
+            ip=self.robot_ip,
         )
         self.lidar_sub = Subscriber(
             "/lidar", LaserScan, self.lidar_callback, ip=self.robot_ip
@@ -86,26 +100,26 @@ class LaptopPilot:
         self.groundtruth_sub = Subscriber(
             "/groundtruth", Pose, self.groundtruth_callback, ip=self.robot_ip
         )
-                    
-    def true_wheel_speeds_callback(self, msg):
-        print("Received sensed wheel speeds: R=", msg.vector.x,", L=", msg.vector.y)
 
-        #update wheel rates
+    def true_wheel_speeds_callback(self, msg):
+        print("Received sensed wheel speeds: R=", msg.vector.x, ", L=", msg.vector.y)
+
+        # update wheel rates
         self.measured_wheelrate_right = msg.vector.x
         self.measured_wheelrate_left = msg.vector.y
         self.datalog.log(msg, topic_name="/true_wheel_speeds")
 
     def lidar_callback(self, msg):
-        # This is a callback function that is called whenever a message is received        
-        print("Received lidar message", msg.header.seq)        
+        # This is a callback function that is called whenever a message is received
+        print("Received lidar message", msg.header.seq)
         if self.sim_init == True:
-            self.sim_time_offset = datetime.utcnow().timestamp()-msg.header.stamp
-            self.sim_init = False     
+            self.sim_time_offset = datetime.utcnow().timestamp() - msg.header.stamp
+            self.sim_init = False
 
         msg.header.stamp += self.sim_time_offset
-        
+
         self.lidar_timestamp_s = msg.header.stamp
-        self.lidar_data = np.zeros((len(msg.ranges.size), 2))
+        self.lidar_data = np.zeros((len(msg.ranges), 2))
         self.lidar_data[:, 0] = msg.ranges
         self.lidar_data[:, 1] = msg.angles
 
@@ -114,41 +128,42 @@ class LaptopPilot:
     def groundtruth_callback(self, msg):
         """This callback receives the odometry ground truth from the simulator."""
         self.datalog.log(msg, topic_name="/groundtruth")
-    
-    def pose_parse(self, msg, aruco = False):
+
+    def pose_parse(self, msg, aruco=False):
         # parser converts pose data to a standard format for logging
         time_stamp = msg[0]
 
         if aruco == True:
             if self.sim_init == True:
-                self.sim_time_offset = datetime.utcnow().timestamp()-msg[0]
-                self.sim_init = False                                         
-                
+                self.sim_time_offset = datetime.utcnow().timestamp() - msg[0]
+                self.sim_init = False
+
             # self.sim_time_offset is 0 if not a simulation. Deals with webots dealing in elapse timeself.sim_time_offset
             print(
                 "Received position update from",
                 datetime.utcnow().timestamp() - msg[0] - self.sim_time_offset,
                 "seconds ago",
             )
-            time_stamp = msg[0] + self.sim_time_offset                
+            time_stamp = msg[0] + self.sim_time_offset
 
-        pose_msg = PoseStamped() 
+        pose_msg = PoseStamped()
         pose_msg.header = Header()
         pose_msg.header.stamp = time_stamp
         pose_msg.pose.position.x = msg[1]
         pose_msg.pose.position.y = msg[2]
         pose_msg.pose.position.z = 0
 
-        quat = Quaternion()        
-        if self.simulation == False and aruco == True: quat.from_euler(0, 0, np.deg2rad(msg[6]))
-        else: quat.from_euler(0, 0, msg[6])
-        pose_msg.pose.orientation = quat        
+        quat = Quaternion()
+        if self.simulation == False and aruco == True:
+            quat.from_euler(0, 0, np.deg2rad(msg[6]))
+        else:
+            quat.from_euler(0, 0, msg[6])
+        pose_msg.pose.orientation = quat
         return pose_msg
-
 
     def run(self, time_to_run=-1):
         self.start_time = datetime.utcnow().timestamp()
-        
+
         try:
             r = Rate(10.0)
             while True:
@@ -174,22 +189,23 @@ class LaptopPilot:
         """
         # > Sense < #
         # get the latest position measurements
-        aruco_pose = self.aruco_driver.read()    
+        aruco_pose = self.aruco_driver.read()
 
-        if aruco_pose is not None:                                                
+        if aruco_pose is not None:
             # converts aruco date to zeroros PoseStamped format
-            msg = self.pose_parse(aruco_pose, aruco = True)
+            msg = self.pose_parse(aruco_pose, aruco=True)
 
-            # reads sensed pose for local use 
+            # reads sensed pose for local use
             self.measured_pose_timestamp_s = msg.header.stamp
             self.measured_pose_northings_m = msg.pose.position.x
             self.measured_pose_eastings_m = msg.pose.position.y
-            _, _, self.measured_pose_yaw_rad = msg.pose.orientation.to_euler()        
-            self.measured_pose_yaw_rad = self.measured_pose_yaw_rad % (np.pi*2) # manage angle wrapping
+            _, _, self.measured_pose_yaw_rad = msg.pose.orientation.to_euler()
+            self.measured_pose_yaw_rad = self.measured_pose_yaw_rad % (
+                np.pi * 2
+            )  # manage angle wrapping
 
-            # logs the data            
+            # logs the data
             self.datalog.log(msg, topic_name="/aruco")
-
 
         # > Think < #
         ################################################################################
@@ -197,8 +213,18 @@ class LaptopPilot:
         self.est_pose_northings_m = 0
         self.est_pose_eastings_m = 0
         self.est_pose_yaw_rad = 0
-        
-        msg = self.pose_parse([datetime.utcnow().timestamp(),self.est_pose_northings_m,self.est_pose_eastings_m,0,0,0,self.est_pose_yaw_rad])
+
+        msg = self.pose_parse(
+            [
+                datetime.utcnow().timestamp(),
+                self.est_pose_northings_m,
+                self.est_pose_eastings_m,
+                0,
+                0,
+                0,
+                self.est_pose_yaw_rad,
+            ]
+        )
         self.datalog.log(msg, topic_name="/est_pose")
         ################################################################################
         #  TODO: Implement your controller here                                        #
@@ -212,7 +238,7 @@ class LaptopPilot:
         ################################################################################
 
         # > Act < #
-        # Send commands to the robot        
+        # Send commands to the robot
         self.wheel_speed_pub.publish(wheel_speed_msg)
         self.datalog.log(wheel_speed_msg, topic_name="/wheel_speeds_cmd")
 
