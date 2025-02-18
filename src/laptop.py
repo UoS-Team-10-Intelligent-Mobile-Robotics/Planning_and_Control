9c3e31c5120538378ef4c5ef553095d1bccdc85b
Nadia <55548630+NadiaHoque@users.noreply.github.com>
2025-02-18 03:58:41 +0000

    laptop.py
    Latest version of laptop.py ran in simulation and not getting the desired output. Would like to discuss in tomorrow's pre-lab group work session regarding what I am doing wrong.

--- src/laptop.py
+++ src/laptop.py
@@ -1,10 +1,3 @@
-"""
-Copyright (c) 2023 The uos_sess6072_build Authors.
-Authors: Miquel Massot, Blair Thornton, Sam Fenton
-All rights reserved.
-Licensed under the BSD 3-Clause License.
-See LICENSE.md file in the project root for full license information.
-"""
 import numpy as np
 import argparse
 from datetime import datetime
@@ -14,68 +7,66 @@ from zeroros import Subscriber, Publisher
 from zeroros.messages import LaserScan, Vector3Stamped, Pose, PoseStamped, Header, Quaternion
 from zeroros.datalogger import DataLogger
 from zeroros.rate import Rate
-# add more libraries here
+from model_feeg6043 import ActuatorConfiguration
+from math_feeg6043 import Vector
+from model_feeg6043 import rigid_body_kinematics
+from model_feeg6043 import RangeAngleKinematics
 
 class LaptopPilot:
     def __init__(self, simulation):
-        # network for sensed pose
         aruco_params = {
-            "port": 50000,  # Port to listen to (DO NOT CHANGE)
-            "marker_id": 25,  # Marker ID to listen to (CHANGE THIS to your marker ID)            
+            "port": 50000,
+            "marker_id": 25,
         }
         self.robot_ip = "192.168.90.1"
-        
-        # handles different time reference, network amd aruco parameters for simulator
-        self.sim_time_offset = 0 #used to deal with webots timestamps
-        self.sim_init = False #used to deal with webots timestamps
+        self.sim_time_offset = 0
+        self.sim_init = False
         self.simulation = simulation
         if self.simulation:
-            self.robot_ip = "127.0.0.1"          
-            aruco_params['marker_id'] = 0  #Ovewrites Aruco marker ID to 0 (needed for simulation)
-            self.sim_init = True #used to deal with webots timestamps
+            self.robot_ip = "127.0.0.1"
+            aruco_params['marker_id'] = 0
+            self.sim_init = True
 
         print("Connecting to robot with IP", self.robot_ip)
         self.aruco_driver = ArUcoUDPDriver(aruco_params, parent=self)
 
-        ############# INITIALISE ATTRIBUTES ##########        
-        # path
+        self.initialise_pose = True
         self.northings_path = []
-        self.eastings_path = []        
+        self.eastings_path = []
 
-        # model pose
         self.est_pose_northings_m = None
         self.est_pose_eastings_m = None
         self.est_pose_yaw_rad = None
 
-        # measured pose
         self.measured_pose_timestamp_s = None
         self.measured_pose_northings_m = None
         self.measured_pose_eastings_m = None
         self.measured_pose_yaw_rad = None
 
-        # wheel speed commands
         self.cmd_wheelrate_right = None
-        self.cmd_wheelrate_left = None 
+        self.cmd_wheelrate_left = None
 
-        # encoder/actual wheel speeds
         self.measured_wheelrate_right = None
-        self.measured_wheelrate_left = None   
+        self.measured_wheelrate_left = None
 
-        # lidar
         self.lidar_timestamp_s = None
         self.lidar_data = None
-        ###############################################################        
+        lidar_xb = 0.0
+        lidar_yb = 0.0
+        self.lidar = RangeAngleKinematics(lidar_xb, lidar_yb)
+
+        wheel_distance = 0.160
+        wheel_diameter = 0.074
+        self.ddrive = ActuatorConfiguration(wheel_distance, wheel_diameter)
 
         self.datalog = DataLogger(log_dir="logs")
 
-        # Wheels speeds in rad/s are encoded as a Vector3 with timestamp, 
-        # with x for the right wheel and y for the left wheel.        
         self.wheel_speed_pub = Publisher(
             "/wheel_speeds_cmd", Vector3Stamped, ip=self.robot_ip
         )
 
         self.true_wheel_speed_sub = Subscriber(
-            "/true_wheel_speeds",Vector3Stamped, self.true_wheel_speeds_callback,ip=self.robot_ip,
+            "/true_wheel_speeds", Vector3Stamped, self.true_wheel_speeds_callback, ip=self.robot_ip,
         )
         self.lidar_sub = Subscriber(
             "/lidar", LaserScan, self.lidar_callback, ip=self.robot_ip
@@ -83,69 +74,78 @@ class LaptopPilot:
         self.groundtruth_sub = Subscriber(
             "/groundtruth", Pose, self.groundtruth_callback, ip=self.robot_ip
         )
-                    
-    def true_wheel_speeds_callback(self, msg):
-        print("Received sensed wheel speeds: R=", msg.vector.x,", L=", msg.vector.y)
 
-        #update wheel rates
+    def true_wheel_speeds_callback(self, msg):
+        print("Received sensed wheel speeds: R=", msg.vector.x, ", L=", msg.vector.y)
         self.measured_wheelrate_right = msg.vector.x
         self.measured_wheelrate_left = msg.vector.y
         self.datalog.log(msg, topic_name="/true_wheel_speeds")
 
     def lidar_callback(self, msg):
-        # This is a callback function that is called whenever a message is received        
-        print("Received lidar message", msg.header.seq)        
-        if self.sim_init == True:
-            self.sim_time_offset = datetime.utcnow().timestamp()-msg.header.stamp
-            self.sim_init = False     
+        """Process Lidar data and convert to Earth frame."""
+        print("Received lidar message", msg.header.seq)
+
+        if self.sim_init:
+            self.sim_time_offset = datetime.utcnow().timestamp() - msg.header.stamp
+            self.sim_init = False
 
         msg.header.stamp += self.sim_time_offset
-        
         self.lidar_timestamp_s = msg.header.stamp
-        self.lidar_data = np.zeros((len(msg.ranges.size), 2))
-        self.lidar_data[:, 0] = msg.ranges
-        self.lidar_data[:, 1] = msg.angles
 
+        p_eb = Vector(3)
+        p_eb[0] = self.est_pose_northings_m
+        p_eb[1] = self.est_pose_eastings_m
+        p_eb[2] = self.est_pose_yaw_rad
+
+        self.lidar_data = np.zeros((len(msg.ranges), 2))
+        z_lm = Vector(2)
+
+        for i in range(len(msg.ranges)):
+            z_lm[0] = msg.ranges[i]
+            z_lm[1] = msg.angles[i]
+            t_em = self.lidar.rangeangle_to_loc(p_eb, z_lm)
+            self.lidar_data[i, 0] = t_em[0]
+            self.lidar_data[i, 1] = t_em[1]
+
+        self.lidar_data = self.lidar_data[~np.isnan(self.lidar_data).any(axis=1)]
         self.datalog.log(msg, topic_name="/lidar")
 
     def groundtruth_callback(self, msg):
-        """This callback receives the odometry ground truth from the simulator."""
         self.datalog.log(msg, topic_name="/groundtruth")
-    
-    def pose_parse(self, msg, aruco = False):
-        # parser converts pose data to a standard format for logging
+
+    def pose_parse(self, msg, aruco=False):
         time_stamp = msg[0]
 
-        if aruco == True:
-            if self.sim_init == True:
-                self.sim_time_offset = datetime.utcnow().timestamp()-msg[0]
-                self.sim_init = False                                         
-                
-            # self.sim_time_offset is 0 if not a simulation. Deals with webots dealing in elapse timeself.sim_time_offset
+        if aruco:
+            if self.sim_init:
+                self.sim_time_offset = datetime.utcnow().timestamp() - msg[0]
+                self.sim_init = False
+
             print(
                 "Received position update from",
                 datetime.utcnow().timestamp() - msg[0] - self.sim_time_offset,
                 "seconds ago",
             )
-            time_stamp = msg[0] + self.sim_time_offset                
+            time_stamp = msg[0] + self.sim_time_offset
 
-        pose_msg = PoseStamped() 
+        pose_msg = PoseStamped()
         pose_msg.header = Header()
         pose_msg.header.stamp = time_stamp
         pose_msg.pose.position.x = msg[1]
         pose_msg.pose.position.y = msg[2]
         pose_msg.pose.position.z = 0
 
-        quat = Quaternion()        
-        if self.simulation == False and aruco == True: quat.from_euler(0, 0, np.deg2rad(msg[6]))
-        else: quat.from_euler(0, 0, msg[6])
-        pose_msg.pose.orientation = quat        
+        quat = Quaternion()
+        if not self.simulation and aruco:
+            quat.from_euler(0, 0, np.deg2rad(msg[6]))
+        else:
+            quat.from_euler(0, 0, msg[6])
+        pose_msg.pose.orientation = quat
         return pose_msg
-
 
     def run(self, time_to_run=-1):
         self.start_time = datetime.utcnow().timestamp()
-        
+
         try:
             r = Rate(10.0)
             while True:
@@ -165,54 +165,64 @@ class LaptopPilot:
             self.true_wheel_speed_sub.stop()
 
     def infinite_loop(self):
-        """Main control loop
+        aruco_pose = self.aruco_driver.read()
 
-        Your code should go here.
-        """
-        # > Sense < #
-        # get the latest position measurements
-        aruco_pose = self.aruco_driver.read()    
-
-        if aruco_pose is not None:                                                
-            # converts aruco date to zeroros PoseStamped format
-            msg = self.pose_parse(aruco_pose, aruco = True)
-
-            # reads sensed pose for local use 
+        if aruco_pose is not None:
+            msg = self.pose_parse(aruco_pose, aruco=True)
             self.measured_pose_timestamp_s = msg.header.stamp
             self.measured_pose_northings_m = msg.pose.position.x
             self.measured_pose_eastings_m = msg.pose.position.y
-            _, _, self.measured_pose_yaw_rad = msg.pose.orientation.to_euler()        
-            self.measured_pose_yaw_rad = self.measured_pose_yaw_rad % (np.pi*2) # manage angle wrapping
+            _, _, self.measured_pose_yaw_rad = msg.pose.orientation.to_euler()
+            self.measured_pose_yaw_rad = self.measured_pose_yaw_rad % (np.pi * 2)
 
-            # logs the data            
+            if self.initialise_pose:
+                self.est_pose_northings_m = self.measured_pose_northings_m
+                self.est_pose_eastings_m = self.measured_pose_eastings_m
+                self.est_pose_yaw_rad = self.measured_pose_yaw_rad
+                self.t_prev = datetime.utcnow().timestamp()
+                self.t = 0
+                time.sleep(0.1)
+                self.initialise_pose = False
+
+            q = Vector(2)
+            if self.measured_wheelrate_right is not None and self.measured_wheelrate_left is not None:
+                q[0] = self.measured_wheelrate_right
+                q[1] = self.measured_wheelrate_left
+                u = self.ddrive.fwd_kinematics(q)
+                print("Twist motion (linear velocity, angular velocity):", u)
+            else:
+                print("Warning: Wheel rate measurements not available yet!")
+                u = Vector(2)
+
             self.datalog.log(msg, topic_name="/aruco")
 
+            if not self.initialise_pose:
+                t_now = datetime.utcnow().timestamp()
+                dt = t_now - self.t_prev
+                self.t += dt
+                self.t_prev = t_now
 
-        # > Think < #
-        ################################################################################
-        #  TODO: Implement your state estimation
-        self.est_pose_northings_m = 0
-        self.est_pose_eastings_m = 0
-        self.est_pose_yaw_rad = 0
-        
-        msg = self.pose_parse([datetime.utcnow().timestamp(),self.est_pose_northings_m,self.est_pose_eastings_m,0,0,0,self.est_pose_yaw_rad])
-        self.datalog.log(msg, topic_name="/est_pose")
-        ################################################################################
-        #  TODO: Implement your controller here                                        #
+                p_robot = Vector(3)
+                p_robot[0, 0] = self.est_pose_northings_m
+                p_robot[1, 0] = self.est_pose_eastings_m
+                p_robot[2, 0] = self.est_pose_yaw_rad
+
+                p_robot = rigid_body_kinematics(p_robot, u, dt)
+                p_robot[2] = p_robot[2] % (2 * np.pi)
+
+                self.est_pose_northings_m = p_robot[0]
+                self.est_pose_eastings_m = p_robot[1]
+                self.est_pose_yaw_rad = p_robot[2]
 
         wheel_speed_msg = Vector3Stamped()
-        wheel_speed_msg.vector.x = 1 * np.pi  # Right wheel 1 rev/s = 1*pi rad/s
-        wheel_speed_msg.vector.y = 2 * np.pi  # Left wheel 1 rev/s = 2*pi rad/s
+        wheel_speed_msg.vector.x = 1 * np.pi
+        wheel_speed_msg.vector.y = 2 * np.pi
 
         self.cmd_wheelrate_right = wheel_speed_msg.vector.x
         self.cmd_wheelrate_left = wheel_speed_msg.vector.y
-        ################################################################################
 
-        # > Act < #
-        # Send commands to the robot        
         self.wheel_speed_pub.publish(wheel_speed_msg)
         self.datalog.log(wheel_speed_msg, topic_name="/wheel_speeds_cmd")
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
